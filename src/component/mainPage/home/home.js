c68aa505a6fa6c33c15146ee0d5295dedf50ae2b
guangyin <2899173331@qq.com>
2022-09-08 18:34:16 +0800

    初次写tabbar

--- src/component/mainPage/home/home.js
+++ src/component/mainPage/home/home.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import MySwiper from './swiper'
+import React from "react";
+import MySwiper from "./swiper";
+import "../../../index.less";
 
 export default class Smallhome extends React.Component {
   render() {
     return (
       <div>
         <MySwiper />
+        <div className="smallhome-tabbar">
+          <div className="item">
+            <img src="../../../../assets/nav-1.png" alt="" />
+            123
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
--- src/component/mainPage/home/swiper.js
+++ src/component/mainPage/home/swiper.js
@@ -1,67 +1,70 @@
-import React from 'react'
-import { Swiper, Toast } from 'antd-mobile'
+import React from "react";
+import { Swiper, Toast } from "antd-mobile";
 
-const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac']
+const colors = ["#ace0ff", "#bcffbd", "#e4fabd", "#ffcfac"];
 
 const items = colors.map((color, index) => (
   <Swiper.Item key={index}>
     <div
-      style={{ background: color, height: 212, fontSize: 20, textAlign: 'center', lineHeight: '212px' }}
+      style={{
+        background: color,
+        height: 212,
+        fontSize: 20,
+        textAlign: "center",
+        lineHeight: "212px",
+      }}
       onClick={() => {
-        Toast.show(`你点击了卡片 ${index + 1}`)
+        Toast.show(`你点击了卡片 ${index + 1}`);
       }}
     >
       carousel
     </div>
   </Swiper.Item>
-))
+));
 
 class MySwiper extends React.Component {
   state = {
     swipers: items,
     test: 1,
-  }
+  };
   getSwipers = async () => {
-    const { data } = await this.$http.get('home/swiper')
+    const { data } = await this.$http.get("home/swiper");
     if (data.status === 200) {
       this.setState(
         {
           swipers: data.body,
         },
         () => {
-          this.renderSwipers()
+          this.renderSwipers();
         }
-      )
+      );
     }
-  }
+  };
   renderSwipers = () => {
     return this.state.swipers.map((item, index) => (
       <Swiper.Item key={index}>
         <div
-          style={{ height: 212, fontSize: 20, textAlign: 'center', lineHeight: '212px' }}
+          style={{
+            height: 212,
+            fontSize: 20,
+            textAlign: "center",
+            lineHeight: "212px",
+          }}
           onClick={() => {
-            Toast.show(`你点击了卡片 ${index + 1}`)
+            Toast.show(`你点击了卡片 ${index + 1}`);
           }}
         >
-          <img style={{ width: '100%', height: '100%' }} src={`${this.$baseUrl}${item.imgSrc}`} alt=""></img>
+          <img
+            style={{ width: "100%", height: "100%" }}
+            src={`${this.$baseUrl}${item.imgSrc}`}
+            alt=""
+          ></img>
         </div>
       </Swiper.Item>
-    ))
-
-    // this.setState(
-    //   {
-    //     swipers: temp,
-    //   },
-    //   () => {
-    //     console.log(this.state.swipers)
-    //     console.log(items)
-    //     console.log(typeof this.state.swipers)
-    //     console.log(typeof items)
-    //   }
-    // )
-  }
+    ));
+  };
   componentDidMount() {
-    this.getSwipers()
+    this.getSwipers();
   }
   render() {
     return (
@@ -70,8 +73,8 @@ class MySwiper extends React.Component {
           {this.renderSwipers()}
         </Swiper>
       </div>
-    )
+    );
   }
 }
 
-export default MySwiper
+export default MySwiper;
