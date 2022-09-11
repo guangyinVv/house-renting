d8416df570976de3280b0bc3c775aa2ee14fc7c1
guangyin <2899173331@qq.com>
2022-09-11 11:26:19 +0800

    渲染城市列表完成

--- src/pages/CityList/index.js
+++ src/pages/CityList/index.js
@@ -1,58 +1,101 @@
-import React from "react";
-import { NavBar } from "antd-mobile";
+import React from 'react'
+import { NavBar } from 'antd-mobile'
+import { getCurrentCity } from '../../utils/index'
+import { List, AutoSizer } from 'react-virtualized'
+import 'react-virtualized/styles.css'
+
+const formatCityIndex = (letter) => {
+  switch (letter) {
+    case '#':
+      return '当前定位'
+    case 'hot':
+      return '热门城市'
+    default:
+      return letter.toUpperCase(letter)
+  }
+}
 
 const formatCityData = (list) => {
-  let cityList = {};
-  let cityIndex = [];
+  let cityList = {}
+  let cityIndex = []
   list.forEach((item) => {
-    const first = item.short.substr(0, 1);
+    const first = item.short.substr(0, 1)
     // 如果有这个分类
     if (cityList[first]) {
-      cityList[first].push(item);
+      cityList[first].push(item)
     } else {
-      cityList[first] = [item];
+      cityList[first] = [item]
     }
-  });
-  cityIndex = Object.keys(cityList).sort();
+  })
+  cityIndex = Object.keys(cityList).sort()
   return {
     cityList,
     cityIndex,
-  };
-};
+  }
+}
 
 export default class CityList extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
       cityList: [],
       cityIndex: [],
-    };
-    this.getCityList();
+    }
+    this.getCityList()
   }
+
+  // 动态得到RowHeight
+  getRowHeight = ({ index }) => {
+    const TITLE_HEIGHT = 38
+    const CITY_HEIGHT = 50
+    return TITLE_HEIGHT + CITY_HEIGHT * this.state.cityList[this.state.cityIndex[index]].length
+  }
+
+  // react-virtualized组件的函数
+  rowRenderer = ({ key, index, isScrolling, isVisible, style }) => {
+    const { cityIndex, cityList } = this.state
+    const letter = cityIndex[index]
+    return (
+      <div key={key} style={style}>
+        <div className="cityIndex">{formatCityIndex(letter)}</div>
+        {cityList[letter].map((item, index) => (
+          <div key={item.value} className="cityName">
+            {item.label}
+          </div>
+        ))}
+      </div>
+    )
+  }
+
   // 得到城市列表数据
   async getCityList() {
-    const { data } = await this.$http.get("/area/city?level=1");
+    const { data } = await this.$http.get('/area/city?level=1')
     if (data.status === 200) {
-      const { cityList, cityIndex } = formatCityData(data.body);
-      const { data: res } = await this.$http.get("/area/hot");
+      const { cityList, cityIndex } = formatCityData(data.body)
+      const { data: res } = await this.$http.get('/area/hot')
       if (res.status === 200) {
-        cityList["hot"] = res.body;
-        cityIndex.unshift("hot");
+        cityList['hot'] = res.body
+        cityIndex.unshift('hot')
+        cityList['#'] = [await getCurrentCity()]
+        cityIndex.unshift('#')
         this.setState({
           cityList: cityList,
           cityIndex: cityIndex,
-        });
+        })
       }
-      console.log(cityList);
-      console.log(cityIndex);
+      console.log(cityList)
+      console.log(cityIndex)
     }
   }
-  back = () => window.history.go(-1);
+  back = () => window.history.go(-1)
   render() {
     return (
       <div>
         <NavBar onBack={this.back}>城市选择</NavBar>
+        <div className="cityList" style={{ width: '100vw', height: 'calc(100vh - 45px)' }}>
+          <AutoSizer>{({ width, height }) => <List width={width} height={height} rowCount={this.state.cityIndex.length} rowHeight={this.getRowHeight} rowRenderer={this.rowRenderer}></List>}</AutoSizer>
+        </div>
       </div>
-    );
+    )
   }
 }
