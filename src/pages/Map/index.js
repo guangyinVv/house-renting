e6dd20b71a726a78e6fdaa777f38acfddd216687
guangyin <2899173331@qq.com>
2022-09-12 21:26:36 +0800

    添加了动态更新覆盖物的功能

--- src/pages/Map/index.js
+++ src/pages/Map/index.js
@@ -5,53 +5,102 @@ export default class Map extends React.Component {
   state = {
     city: "",
   };
+
+  // 地图的缩放等级
+  countLevel = 11;
+
+  // 实现动态创建覆盖物的函数
+  renderOverlays = async (map, value, point) => {
+    // point 是包含lng，lat的对象，显示当前城市的经纬度
+    const { data } = await this.$http.get(`/area/map?id=${value}`);
+
+    map.centerAndZoom(point, this.countLevel);
+    // setTimeout(() => {
+    console.log(data.body);
+    data.body.forEach((item) => {
+      const { coord, value, label: name, count } = item;
+      const { longitude, latitude } = coord;
+      const opts = {
+        position: {
+          lng: longitude,
+          lat: latitude,
+        },
+        offset: new window.BMapGL.Size(-35, -35),
+      };
+      const label = new window.BMapGL.Label("", opts);
+      label.setContent(
+        `<div class="overlay"><div class="name">${name}</div><div class="num">${count}套</div></div>`
+      );
+      label.setStyle({
+        border: "solid 0px white",
+        padding: 0,
+        whiteSpace: "nowrap",
+      });
+      label.addEventListener("click", () => {
+        this.countLevel += 2;
+        // map.centerAndZoom(opts.position, this.countLevel);
+        map.clearOverlays();
+        this.renderOverlays(map, value, opts.position);
+      });
+      map.addOverlay(label);
+    });
+    // }, 0);
+  };
+
   async componentDidMount() {
     const map = new window.BMapGL.Map("container");
-    const { label } = JSON.parse(localStorage.getItem("hkzf_city"));
-    const { data } = await this.$http.get(`/area/info?name=${label}`);
+    const { label, value } = JSON.parse(localStorage.getItem("hkzf_city"));
     // 给地图添加比例尺和缩放控件
     map.addControl(new window.BMapGL.ScaleControl());
     map.addControl(new window.BMapGL.ZoomControl());
     // 把有数据的城市给展示出来
-    if (data.status === 200) {
-      this.setState(
-        {
-          city: data.body.label,
-        },
-        () => {
-          // 解析城市的地址
-          var myGeo = new window.BMapGL.Geocoder();
-          // 将地址解析结果显示在地图上，并调整地图视野
-          myGeo.getPoint(
-            label,
-            function (point) {
-              if (point) {
-                map.centerAndZoom(point, 11);
-                // 添加文本覆盖物
-                const opts = {
-                  position: point,
-                  offset: new window.BMapGL.Size(-35, -35),
-                };
-                const label = new window.BMapGL.Label("", opts);
+    this.setState(
+      {
+        city: { label, value },
+      },
+      () => {
+        // 解析城市的地址
+        var myGeo = new window.BMapGL.Geocoder();
+        // 将地址解析结果显示在地图上，并调整地图视野
+        myGeo.getPoint(
+          label,
+          async (point) => {
+            this.renderOverlays(map, value, point);
+            // point 是包含lng，lat的对象，显示当前城市的经纬度
+            // const { data } = await this.$http.get(`/area/map?id=${value}`);
 
-                label.setContent(
-                  '<div class="overlay"><div class="name">浦东</div><div class="num">99套</div></div>'
-                );
-                label.setStyle({
-                  border: "solid 0px white",
-                  padding: 0,
-                  whiteSpace: "nowrap",
-                });
-                map.addOverlay(label);
-              } else {
-                alert("您选择的地址没有解析到结果！");
-              }
-            },
-            label
-          );
-        }
-      );
-    }
+            // map.centerAndZoom(point, 11);
+            // data.body.forEach((item) => {
+            //   const { coord, value, label: name, count } = item;
+            //   const { longitude, latitude } = coord;
+            //   const opts = {
+            //     position: {
+            //       lng: longitude,
+            //       lat: latitude,
+            //     },
+            //     offset: new window.BMapGL.Size(-35, -35),
+            //   };
+            //   const label = new window.BMapGL.Label("", opts);
+            //   label.setContent(
+            //     `<div class="overlay"><div class="name">${name}</div><div class="num">${count}套</div></div>`
+            //   );
+            //   label.setStyle({
+            //     border: "solid 0px white",
+            //     padding: 0,
+            //     whiteSpace: "nowrap",
+            //   });
+            //   label.addEventListener("click", function () {
+            //     console.log("点击了", value);
+            //     map.centerAndZoom(opts.position, 13);
+            //     map.clearOverlays();
+            //   });
+            //   map.addOverlay(label);
+            // });
+          },
+          label
+        );
+      }
+    );
   }
   render() {
     return (
