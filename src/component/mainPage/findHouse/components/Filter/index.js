2e8666783b7c120eb4bec7f92a7bebf8f52a566b
guangyin <2899173331@qq.com>
2022-09-17 17:39:40 +0800

    完成了筛选部分

--- src/component/mainPage/findHouse/components/Filter/index.js
+++ src/component/mainPage/findHouse/components/Filter/index.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import FilterTitle from '../FilterTitle'
-import FilterPicker from '../FilterPicker'
-import styles from './index.module.css'
-import { Button } from 'antd-mobile'
-import FilterMore from '../FilterMore'
+import React from "react";
+import FilterTitle from "../FilterTitle";
+import FilterPicker from "../FilterPicker";
+import styles from "./index.module.css";
+import { Button } from "antd-mobile";
+import FilterMore from "../FilterMore";
 
 export default class Filter extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       titleSelectedStatus: {
         area: false,
@@ -18,127 +18,152 @@ export default class Filter extends React.Component {
       gotPickerValue: [],
       // 控制FilterPicker 或 FilterMore 组件的展示和隐藏
       // area / way / money / select
-      openType: '',
+      openType: "",
       // 通过接口获取的所有数据
-      filtersData: '',
+      filtersData: "",
       pickerData: [],
       // 用于展示选择器视图的默认内容
       defaultList: {},
       // 初始展示的数据（数组，每项只有一个或三个）
       defaultData: {},
-    }
-    this.getFiltersData()
+    };
+    this.getFiltersData();
   }
 
   async getFiltersData() {
-    const { value } = JSON.parse(localStorage.getItem('hkzf_city'))
-    const { data } = await this.$http.get(`/houses/condition?id=${value}`)
+    const { value } = JSON.parse(localStorage.getItem("hkzf_city"));
+    const { data } = await this.$http.get(`/houses/condition?id=${value}`);
     this.setState({
       filtersData: data.body,
-    })
+    });
   }
 
   componentDidUpdate() {}
 
+  // 渲染筛选部分
+  renderFilterMore() {
+    const {
+      openType,
+      filtersData: { roomType, oriented, floor, characteristic },
+    } = this.state;
+    const data = {
+      roomType,
+      oriented,
+      floor,
+      characteristic,
+    };
+    if (openType !== "select") return null;
+    else
+      return (
+        <FilterMore
+          defaultData={this.state.gotPickerValue[3]}
+          sendValue={this.getValue}
+          data={data}
+        />
+      );
+  }
+
   findDataByValue(data, value) {
-    let temp
+    let temp;
     data.some((item) => {
       if (item.value === value) {
-        temp = item
-        return true
+        temp = item;
+        return true;
       }
-      return false
-    })
-    return temp
+      return false;
+    });
+    return temp;
   }
 
   // 初始化默认选择项
   initDefaultData() {
-    const { gotPickerValue: defaultData, pickerData: data, openType: type } = this.state
+    const {
+      gotPickerValue: defaultData,
+      pickerData: data,
+      openType: type,
+    } = this.state;
 
     if (data.length === 0) {
-      return false
+      return false;
     }
 
-    let index
+    let index;
     switch (type) {
-      case 'area':
-        index = 0
-        break
-      case 'way':
-        index = 1
-        break
-      case 'money':
-        index = 2
-        break
+      case "area":
+        index = 0;
+        break;
+      case "way":
+        index = 1;
+        break;
+      case "money":
+        index = 2;
+        break;
       default:
-        index = 3
-        break
+        index = 3;
+        break;
     }
     if (index === 3) {
-      return
+      return;
     }
-    let myData = defaultData[index]
+    let myData = defaultData[index];
     if (defaultData.length === 0 || myData.length === 0) {
-      return false
+      return false;
     } else {
-      let temp = []
-      let tempData = []
-      if (type === 'area') {
+      let temp = [];
+      let tempData = [];
+      if (type === "area") {
         // 第0项
-        tempData[0] = data[index]
-        temp[0] = this.findDataByValue(tempData[0], myData[0])
-        tempData[1] = temp[0].children
-        temp[1] = this.findDataByValue(tempData[1], myData[1])
-        tempData[2] = temp[1].children
+        tempData[0] = data[index];
+        temp[0] = this.findDataByValue(tempData[0], myData[0]);
+        tempData[1] = temp[0].children;
+        temp[1] = this.findDataByValue(tempData[1], myData[1]);
+        tempData[2] = temp[1].children;
         try {
-          temp[2] = this.findDataByValue(tempData[2], myData[2])
+          temp[2] = this.findDataByValue(tempData[2], myData[2]);
         } catch (error) {
           tempData[2] = [
             {
-              label: '不限',
-              value: 'null',
+              label: "不限",
+              value: "null",
             },
-          ]
-          temp[2] = 'null'
+          ];
+          temp[2] = "null";
         }
 
         for (var i = 0; i < temp.length; i++) {
-          temp[i] = temp[i].value
+          temp[i] = temp[i].value;
         }
-        let { ...defaultList } = this.state.defaultList
-        defaultList[type] = tempData
-        let { ...defaultData } = this.state.defaultData
-        defaultData[type] = temp
+        let { ...defaultList } = this.state.defaultList;
+        defaultList[type] = tempData;
+        let { ...defaultData } = this.state.defaultData;
+        defaultData[type] = temp;
         this.setState({
-          // data: tempData,
-          // defaultData: temp,
           defaultList: defaultList,
           defaultData: defaultData,
-        })
+        });
       } else {
-        let { ...defaultData } = this.state.defaultData
-        defaultData[type] = this.state.gotPickerValue[index]
+        let { ...defaultData } = this.state.defaultData;
+        defaultData[type] = this.state.gotPickerValue[index];
         this.setState({
           defaultData: defaultData,
-        })
+        });
       }
-      return true
+      return true;
     }
   }
 
   // 点击标题高亮
   onTitleClick = (type) => {
     // console.log(type);
-    const { area, rentType, subway, price } = this.state.filtersData
-    let flag = this.state.titleSelectedStatus[type]
-    let data
-    if (type === 'area') {
-      data = [[area, subway]]
-    } else if (type === 'way') {
-      data = [rentType]
-    } else if (type === 'money') {
-      data = [price]
+    const { area, rentType, subway, price } = this.state.filtersData;
+    let flag = this.state.titleSelectedStatus[type];
+    let data;
+    if (type === "area") {
+      data = [[area, subway]];
+    } else if (type === "way") {
+      data = [rentType];
+    } else if (type === "money") {
+      data = [price];
     }
     // 如果点中的这一项中没有选中内容
     if (flag === false) {
@@ -149,29 +174,44 @@ export default class Filter extends React.Component {
         },
         openType: type,
         pickerData: data,
-      })
+      });
     } else {
       // 如果点中的这一项中有选中内容
       this.setState({
         openType: type,
         pickerData: data,
-      })
+      });
     }
-  }
+  };
 
   // 隐藏对话框
   onCancel = () => {
-    this.setState({
-      defaultData: { ...this.state.defaultData, [this.state.openType]: [] },
-      defaultList: { ...this.state.defaultList, [this.state.openType]: [] },
-      openType: '',
-      titleSelectedStatus: {
-        ...this.state.titleSelectedStatus,
-        [this.state.openType]: false,
-      },
-    })
-  }
+    const { gotPickerValue, openType } = this.state;
 
+    if (openType === "select") {
+      if (gotPickerValue[3] === undefined || gotPickerValue[3].length === 0) {
+        this.setState({
+          titleSelectedStatus: {
+            ...this.state.titleSelectedStatus,
+            [openType]: false,
+          },
+        });
+      }
+      this.setState({
+        openType: "",
+      });
+    } else {
+      this.setState({
+        defaultData: { ...this.state.defaultData, [openType]: [] },
+        defaultList: { ...this.state.defaultList, [openType]: [] },
+        openType: "",
+        titleSelectedStatus: {
+          ...this.state.titleSelectedStatus,
+          [openType]: false,
+        },
+      });
+    }
+  };
   // 点击确定按钮
   onSave = () => {
     this.setState(
@@ -182,48 +222,70 @@ export default class Filter extends React.Component {
         },
       },
       () => {
-        this.initDefaultData()
+        this.initDefaultData();
         this.setState({
-          openType: '',
-        })
+          openType: "",
+        });
       }
-    )
-  }
+    );
+  };
 
   // 把该函数给子组件，子组件调用方法把value给父组件
   getValue = (type, value) => {
-    let index
+    let index;
     switch (type) {
-      case 'area':
-        index = 0
-        break
-      case 'way':
-        index = 1
-        break
-      case 'money':
-        index = 2
-        break
+      case "area":
+        index = 0;
+        break;
+      case "way":
+        index = 1;
+        break;
+      case "money":
+        index = 2;
+        break;
       // select
       default:
-        index = 3
-        break
+        index = 3;
+        break;
     }
-    let temp = Array.from(this.state.gotPickerValue)
+    let temp = Array.from(this.state.gotPickerValue);
     // console.log(value)
-    temp[index] = value
+    temp[index] = value;
     this.setState({
       gotPickerValue: temp,
-    })
-  }
+    });
+    if (type === "select") {
+      if (value.length === 0) {
+        this.setState({
+          titleSelectedStatus: {
+            ...this.state.titleSelectedStatus,
+            [this.state.openType]: false,
+          },
+        });
+      }
+      this.setState({
+        openType: "",
+      });
+    }
+  };
 
   render() {
-    const { titleSelectedStatus, openType } = this.state
+    const { titleSelectedStatus, openType } = this.state;
     return (
       <>
-        <FilterTitle titleSelectedStatus={titleSelectedStatus} onTitleClick={this.onTitleClick} />
-        {openType === 'area' || openType === 'way' || openType === 'money' ? (
+        <FilterTitle
+          titleSelectedStatus={titleSelectedStatus}
+          onTitleClick={this.onTitleClick}
+        />
+        {openType === "area" || openType === "way" || openType === "money" ? (
           <div>
-            <FilterPicker data={this.state.pickerData} type={this.state.openType} sendValue={this.getValue} defaultList={this.state.defaultList} defaultData={this.state.defaultData} />
+            <FilterPicker
+              data={this.state.pickerData}
+              type={this.state.openType}
+              sendValue={this.getValue}
+              defaultList={this.state.defaultList}
+              defaultData={this.state.defaultData}
+            />
             {/* 取消，确定按钮 */}
             <div className={styles.buttons}>
               <div className={styles.cancel}>
@@ -237,9 +299,11 @@ export default class Filter extends React.Component {
             </div>
           </div>
         ) : null}
-        {openType === 'select' ? <FilterMore /> : null}
-        {openType !== '' ? <div className={styles.mask}></div> : null}
+        {this.renderFilterMore()}
+        {openType !== "" ? (
+          <div className={styles.mask} onClick={this.onCancel}></div>
+        ) : null}
       </>
-    )
+    );
   }
 }
--- src/component/mainPage/findHouse/components/FilterMore/index.js
+++ src/component/mainPage/findHouse/components/FilterMore/index.js
@@ -1,41 +1,100 @@
-import React from 'react'
-import styles from './index.module.css'
-import { Button } from 'antd-mobile'
+import React from "react";
+import styles from "./index.module.css";
+import { Button } from "antd-mobile";
 
 export default class FilterMore extends React.Component {
-  state = {}
+  constructor(props) {
+    super(props);
+    const { defaultData, data } = props;
+    this.state = {
+      data: data,
+      // 表示当前选中项的值
+      selectedValues: defaultData === undefined ? [] : defaultData,
+    };
+  }
+
+  // 点击标签时触发
+  onTagClick(value) {
+    const { selectedValues } = this.state;
+    const newSelectedValues = [...selectedValues];
+    if (selectedValues.indexOf(value) <= -1) {
+      // 该标签没有被选中
+      newSelectedValues.push(value);
+    } else {
+      // 被选中则取消选中
+      const index = newSelectedValues.findIndex((item) => item === value);
+      newSelectedValues.splice(index, 1);
+    }
+    this.setState({
+      selectedValues: newSelectedValues,
+    });
+  }
+
+  // 渲染标签
+  renderFilters(type) {
+    const { selectedValues } = this.state;
+
+    return type.map((item) => {
+      const isSelected = selectedValues.indexOf(item.value) > -1;
+      return (
+        <div
+          key={item.value}
+          className={[styles.item, isSelected ? styles.active : ""].join(" ")}
+          onClick={() => this.onTagClick(item.value)}
+        >
+          {item.label}
+        </div>
+      );
+    });
+  }
+
+  // 点击取消按钮
+  cancel() {
+    this.setState({
+      selectedValues: [],
+    });
+  }
+  // 点击确定按钮
+  isOk = () => {
+    this.props.sendValue("select", this.state.selectedValues);
+  };
+
   render() {
+    const {
+      data: { roomType, oriented, floor, characteristic },
+    } = this.state;
     return (
       <>
         <div className={styles.more}>
           <div className={styles.title}>户型</div>
           <div className={styles.items}>
-            <div className={`${styles.item} ${styles.active}`}>东北</div>
-            <div className={styles.item}>东北</div>
-            <div className={styles.item}>东北</div>
-            <div className={styles.item}>东北</div>
-            <div className={styles.item}>东北</div>
+            {/* <div className={`${styles.item} ${styles.active}`}>东北</div> */}
+            {this.renderFilters(roomType)}
           </div>
-          <div className={styles.title}>户型</div>
+          <div className={styles.title}>朝向</div>
+          <div className={styles.items}>{this.renderFilters(oriented)}</div>
+          <div className={styles.title}>楼层</div>
+          <div className={styles.items}>{this.renderFilters(floor)}</div>
+          <div className={styles.title}>房屋亮点</div>
           <div className={styles.items}>
-            <div className={styles.item}>东北</div>
-          </div>
-          <div className={styles.title}>户型</div>
-          <div className={styles.items}>
-            <div className={styles.item}>东北</div>
-          </div>
-          <div className={styles.title}>户型</div>
-          <div className={styles.items}>
-            <div className={styles.item}>东北</div>
+            {this.renderFilters(characteristic)}
           </div>
           <div className={styles.buttons}>
-            <Button>取消</Button>
-            <Button color="success">确定</Button>
+            <Button
+              onClick={() => {
+                this.cancel();
+              }}
+            >
+              清除
+            </Button>
+            <Button color="success" onClick={this.isOk}>
+              确定
+            </Button>
           </div>
         </div>
 
         <div className={styles.mask}></div>
       </>
-    )
+    );
   }
 }
