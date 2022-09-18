0040dcc27f7aaa16ba301007e0b8a47433aa67ad
guangyin <2899173331@qq.com>
2022-09-18 14:10:46 +0800

    数据不能更新

--- src/component/mainPage/findHouse/findHouse.js
+++ src/component/mainPage/findHouse/findHouse.js
@@ -1,59 +1,60 @@
-import React from "react";
-import SearchHeader from "../../searchHeader";
-import styles from "./index.module.css";
-import { LeftOutline } from "antd-mobile-icons";
-import Filter from "./components/Filter";
-import HouseList from "../../../pages/HouseList";
+import React from 'react'
+import SearchHeader from '../../searchHeader'
+import styles from './index.module.css'
+import { LeftOutline } from 'antd-mobile-icons'
+import Filter from './components/Filter'
+import HouseList from '../../../pages/HouseList'
 
 export default class FindHouse extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       HouseListData: {},
-    };
+    }
     // 用于展示房屋列表数据的value值
-    this.HouseListValue = {};
+    this.HouseListValue = {}
   }
 
   getHouseListData = (data) => {
-    this.HouseListValue = data;
-    this.searchHouseList();
-  };
+    this.HouseListValue = data
+    this.searchHouseList()
+  }
 
   // 用来获取房屋列表数据
   async searchHouseList() {
-    const { value } = JSON.parse(localStorage.getItem("hkzf_city"));
-    const { data } = await this.$http.get("/houses", {
+    const { value } = JSON.parse(localStorage.getItem('hkzf_city'))
+
+    const { area, mode, price, more } = this.HouseListValue
+    console.log(area)
+
+    const { data } = await this.$http.get('/houses', {
       params: {
         cityId: value,
-        ...this.HouseListValue,
+        area: area,
+        mode: mode,
+        price: price,
+        more: more,
         start: 1,
         end: 20,
       },
-    });
+    })
     this.setState({
       HouseListData: data.body.list,
-    });
+    })
   }
 
   render() {
-    const { HouseListData } = this.state;
     return (
       <>
         <div className={styles.searchBar}>
-          <div
-            className={styles.leftArrow}
-            onClick={() => window.history.go(-1)}
-          >
+          <div className={styles.leftArrow} onClick={() => window.history.go(-1)}>
             <LeftOutline fontSize={20} />
           </div>
           <SearchHeader />
         </div>
         <Filter getHouseListData={this.getHouseListData}></Filter>
-        {HouseListData.length > 0 ? (
-          <HouseList HouseListData={HouseListData} />
-        ) : null}
+        {this.state.HouseListData.length > 0 ? <HouseList HouseListData={this.state.HouseListData} /> : null}
       </>
-    );
+    )
   }
 }
--- src/pages/HouseList/index.js
+++ src/pages/HouseList/index.js
@@ -1,26 +1,22 @@
-import React from "react";
-import styles from "./index.module.css";
+import React from 'react'
+import styles from './index.module.css'
 
 export default class HouseList extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       HouseListData: props.HouseListData,
-    };
+    }
   }
 
   test = () => {
-    const { HouseListData } = this.state;
-    console.log(HouseListData);
+    const { HouseListData } = this.state
+    console.log(HouseListData)
     return HouseListData.map((item, index) => {
       return (
         <div className={styles.item} key={index}>
           <div className={styles.left}>
-            <img
-              className={styles.img}
-              src={`${this.$baseUrl}${item.houseImg}`}
-              alt=""
-            ></img>
+            <img className={styles.img} src={`${this.$baseUrl}${item.houseImg}`} alt=""></img>
           </div>
           <div className={styles.right}>
             <div className={styles.title}>{item.title}</div>
@@ -35,9 +31,9 @@ export default class HouseList extends React.Component {
             </div>
           </div>
         </div>
-      );
-    });
-  };
+      )
+    })
+  }
 
   render() {
     return (
@@ -59,26 +55,8 @@ export default class HouseList extends React.Component {
               <span className={styles.after}>元/月</span>
             </div>
           </div>
-        </div>
-
-        <div className={styles.item}>
-          <div className={styles.left}>
-            <div className={styles.img}></div>
-          </div>
-          <div className={styles.right}>
-            <div className={styles.title}>环球国际 1室1厅 1280元</div>
-            <p className={styles.desc}>1室1厅1卫/99/西/环球国际</p>
-            <div className={styles.labels}>
-              <div className={styles.label}>近地铁</div>
-              <div className={styles.label}>近地铁</div>
-            </div>
-            <div className={styles.price}>
-              <span className={styles.money}>8500</span>
-              <span className={styles.after}>元/月</span>
-            </div>
-          </div>
         </div> */}
       </div>
-    );
+    )
   }
 }
