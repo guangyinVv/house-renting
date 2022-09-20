80a715b1fd774f7ca66e2341f2ce034f3d5340d9
guangyin <2899173331@qq.com>
2022-09-20 21:03:09 +0800

    完成了吸顶功能，优化了加载功能

--- src/component/mainPage/findHouse/findHouse.js
+++ src/component/mainPage/findHouse/findHouse.js
@@ -1,16 +1,17 @@
 import React from "react";
 import SearchHeader from "../../searchHeader";
 import styles from "./index.module.css";
+import { Toast } from "antd-mobile";
 import { LeftOutline } from "antd-mobile-icons";
 import Filter from "./components/Filter";
 import HouseList from "../../../pages/HouseList";
+import Sticky from "../../Sticky";
 
 export default class FindHouse extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       HouseListData: [],
-      unUse: 0,
     };
     // 用于展示房屋列表数据的value值
     this.HouseListValue = {
@@ -19,6 +20,10 @@ export default class FindHouse extends React.Component {
       price: "null",
       more: "null",
     };
+    this.pageInfo = {
+      start: 1,
+      end: 20,
+    };
   }
 
   // 更新默认房屋列表数据
@@ -26,19 +31,27 @@ export default class FindHouse extends React.Component {
     this.searchHouseList();
   }
 
-  getHouseListData = (data) => {
+  // 触发这一函数表明重新获取数据了，需要把page归零
+  getHouseListData = async (data) => {
     this.HouseListValue = data;
-    this.searchHouseList();
+    this.pageInfo = {
+      start: 1,
+      end: 20,
+    };
+    Toast.show({
+      content: "加载中...",
+      icon: "loading",
+    });
+    await this.searchHouseList();
+    Toast.clear();
   };
 
   // 用来获取房屋列表数据
-  async searchHouseList() {
+  searchHouseList = async () => {
     const { value } = JSON.parse(localStorage.getItem("hkzf_city"));
-
     const { area: area1, mode, price, more } = this.HouseListValue;
-
+    const { start, end } = this.pageInfo;
     const area = area1 !== undefined ? area1 : "null";
-
     const { data } = await this.$http.get("/houses", {
       params: {
         cityId: value,
@@ -46,15 +59,35 @@ export default class FindHouse extends React.Component {
         mode: mode,
         price: price,
         more: more,
-        start: 1,
-        end: 20,
+        start: start,
+        end: end,
       },
     });
-    this.setState({
-      HouseListData: data.body.list,
-      unUse: this.state.unUse + 1,
-    });
-  }
+    // 如果是第一次查数据，则重新设置houselistdata的值，否则直接拼接即可
+    if (start === 1) {
+      this.setState({
+        HouseListData: data.body.list,
+      });
+    } else {
+      const temp = this.state.HouseListData;
+      if (data.body.list.length !== 0) {
+        temp.push(...data.body.list);
+        this.setState({
+          HouseListData: temp,
+        });
+      }
+    }
+
+    this.pageInfo = {
+      start: start + data.body.list.length,
+      end: end + data.body.list.length,
+    };
+    if (data.body.list.length === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  };
 
   render() {
     return (
@@ -68,10 +101,12 @@ export default class FindHouse extends React.Component {
           </div>
           <SearchHeader />
         </div>
-        <Filter getHouseListData={this.getHouseListData}></Filter>
+        <Sticky height="42">
+          <Filter getHouseListData={this.getHouseListData}></Filter>
+        </Sticky>
         <HouseList
           HouseListData={this.state.HouseListData}
-          unUse={this.state.unUse}
+          searchHouseList={this.searchHouseList}
         />
       </>
     );
