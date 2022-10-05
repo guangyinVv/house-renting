b4563d8ec76a5c8fcc6e496c9dcd27eb40215c0d
guangyin <2899173331@qq.com>
2022-10-05 22:00:03 +0800

    图片上传部分问题解决了

--- src/component/houseConfig/index.js
+++ src/component/houseConfig/index.js
@@ -8,44 +8,78 @@ const HouseConfig = (props) => {
     {
       name: '衣柜',
       iconClass: 'icon-test4',
+      select: false,
     },
     {
       name: '洗衣机',
       iconClass: 'icon-test3',
+      select: false,
     },
     {
       name: '空调',
       iconClass: 'icon-test5',
+      select: false,
     },
     {
       name: '天然气',
       iconClass: 'icon-test2',
+      select: false,
     },
     {
       name: '冰箱',
       iconClass: 'bingxiang',
+      select: false,
     },
     {
       name: '电视',
       iconClass: 'icon-test',
+      select: false,
     },
     {
       name: '热水器',
       iconClass: 'linyu',
+      select: false,
     },
     {
       name: '沙发',
       iconClass: 'icon-test6',
+      select: false,
     },
     {
       name: '宽带',
       iconClass: 'icon-test1',
+      select: false,
     },
     {
       name: '暖气',
       iconClass: 'FLOOR_HEATING_SYSTEM',
+      select: false,
     },
   ])
+
+  // 选择图标
+  const setSelect = (index) => {
+    let temp = [...icons]
+    temp[index].select = !icons[index].select
+    setIcons(temp)
+  }
+
+  // icons一变化就会触发formatValue函数
+  useEffect(() => {
+    formatValue()
+  }, [icons])
+
+  // 获取图标的value，即在每个name中间加|
+  const formatValue = () => {
+    const array = []
+    icons.forEach((item) => {
+      if (item.select) {
+        array.push(item.name)
+      }
+    })
+    const value = array.join('|')
+    props.onChange(value)
+  }
 
   const selectIcon = () => {
     if (props.list.length === 0) {
@@ -66,9 +100,20 @@ const HouseConfig = (props) => {
   }, [])
 
   const renderIcons = () => {
-    return icons.map((item) => {
+    return icons.map((item, index) => {
       return (
-        <div key={item.iconClass} className={styles.item}>
+        <div
+          key={item.iconClass}
+          className={styles.item}
+          onClick={
+            props.select
+              ? () => {
+                  setSelect(index)
+                }
+              : () => {}
+          }
+          style={item.select ? { color: 'rgb(30 219 160)' } : {}}
+        >
           <span className={`iconfont icon-${item.iconClass}`}></span>
           <div>{item.name}</div>
         </div>
@@ -86,9 +131,12 @@ const HouseConfig = (props) => {
 HouseConfig.propTypes = {
   // list的具体内容应该像这样['宽带', '沙发']
   list: PropTypes.array,
+  select: PropTypes.bool,
+  onChange: PropTypes.func,
 }
 HouseConfig.defaultProps = {
   list: [],
+  onChange: () => {},
 }
 
 export default HouseConfig
--- src/pages/Rent/Add/index.js
+++ src/pages/Rent/Add/index.js
@@ -2,16 +2,55 @@ import MyNavBar from '../../../component/navbar'
 import styles from './index.module.css'
 import WithRightArrow from '../../../component/WithRightArrow'
 import { useState, useEffect } from 'react'
-import { Input, ImageUploader, Button, Picker } from 'antd-mobile'
+import { Input, ImageUploader, Button } from 'antd-mobile'
 import HouseConfig from '../../../component/houseConfig'
 import { useLocation, useNavigate } from 'react-router-dom'
+import { PickerView } from 'antd-mobile'
+import { axios } from '../../../utils/useAxios.js'
 const RentAdd = () => {
+  // 租金
   const [money, setMoney] = useState('')
+  // 建筑面积
   const [area, setArea] = useState('')
+  // 房屋标题
   const [title, setTitle] = useState('')
+  // 房屋描述
   const [desc, setDesc] = useState('')
   // 上传图片
   const [img, setImg] = useState([])
+
+  // 户型
+  const [roomtype, setRoomtype] = useState([])
+  const [roomtypeText, setRoomtypeText] = useState('请选择')
+  useEffect(() => {
+    roomTypeData.forEach((item) => {
+      if (item.value === roomtype[0]) {
+        setRoomtypeText(item.label)
+      }
+    })
+  }, [roomtype])
+  // 所在楼层
+  const [floor, setFloor] = useState([])
+  const [floorText, setFloorText] = useState('请选择')
+  useEffect(() => {
+    floorData.forEach((item) => {
+      if (item.value === floor[0]) {
+        setFloorText(item.label)
+      }
+    })
+  }, [floor])
+  // 朝向
+  const [position, setPosition] = useState([])
+  const [positionText, setPositionText] = useState('请选择')
+  useEffect(() => {
+    orientedData.forEach((item) => {
+      if (item.value === position[0]) {
+        setPositionText(item.label)
+      }
+    })
+  }, [position])
+  // 房屋配置
+  const [houseConfigValue, setHouseConfigValue] = useState('')
 
   // 小区名称
   const [communityName, setCommunityName] = useState({})
@@ -27,6 +66,8 @@ const RentAdd = () => {
     }
   }, [])
 
+  const [visible, setVisible] = useState(false)
+
   // 弹出列表的列表项
   const [pickerList, setPickerList] = useState([
     [
@@ -34,30 +75,66 @@ const RentAdd = () => {
       { label: '22', value: '2' },
     ],
   ])
-  const [visible, setVisible] = useState(false)
+
   const [value, setValue] = useState('')
 
   const uploadImg = (val) => {
-    console.log(val)
+    const url = URL.createObjectURL(val)
+    let temp = val
+    temp.url = url
+    console.log(temp)
     return {
-      url: URL.createObjectURL(val),
+      url: url,
     }
   }
+
+  // 房屋类型
+  const roomTypeData = [
+    { label: '一室', value: 'ROOM|d4a692e4-a177-37fd' },
+    { label: '二室', value: 'ROOM|d1a00384-5801-d5cd' },
+    { label: '三室', value: 'ROOM|20903ae0-c7bc-f2e2' },
+    { label: '四室', value: 'ROOM|ce2a5daa-811d-2f49' },
+    { label: '四室+', value: 'ROOM|2731c38c-5b19-ff7f' },
+  ]
+
+  // 朝向：
+  const orientedData = [
+    { label: '东', value: 'ORIEN|141b98bf-1ad0-11e3' },
+    { label: '西', value: 'ORIEN|103fb3aa-e8b4-de0e' },
+    { label: '南', value: 'ORIEN|61e99445-e95e-7f37' },
+    { label: '北', value: 'ORIEN|caa6f80b-b764-c2df' },
+    { label: '东南', value: 'ORIEN|dfb1b36b-e0d1-0977' },
+    { label: '东北', value: 'ORIEN|67ac2205-7e0f-c057' },
+    { label: '西南', value: 'ORIEN|2354e89e-3918-9cef' },
+    { label: '西北', value: 'ORIEN|80795f1a-e32f-feb9' },
+  ]
+
+  // 楼层
+  const floorData = [
+    { label: '高楼层', value: 'FLOOR|1' },
+    { label: '中楼层', value: 'FLOOR|2' },
+    { label: '低楼层', value: 'FLOOR|3' },
+  ]
+
+  // 用于显示被选中的信息
+  let [currentSelect, setCurrentSelect] = useState('')
+
+  // 用于存传输的图片
+  const [imgs, setImgs] = useState([])
+
+  // const addHouse = () => {
+  //   if (imgs.length > 0) {
+  //     const form = new FormData()
+  //     imgs.forEach((item) => {
+  //       form.append('file', item)
+  //       console.log(form)
+  //     })
+  //     const { data } = axios.post('/houses/image', imgs, { headers: { 'Content-Type': 'multipart/form-data' } })
+  //     console.log(data)
+  //   }
+  // }
   return (
     <div className={styles.divLineBox}>
-      <div>
-        <Picker
-          columns={pickerList}
-          visible={visible}
-          onClose={() => {
-            setVisible(false)
-          }}
-          value={value}
-          onConfirm={(v) => {
-            setValue(v)
-          }}
-        />
-      </div>
       <MyNavBar>发布房源</MyNavBar>
       <div className={styles.title}>房源信息</div>
       <WithRightArrow title="小区名称" onRightClick={() => navigate('/rent/search')} rightArrow={true} rightText={communityName === {} || communityName.name === undefined ? '请选择小区' : communityName.name}></WithRightArrow>
@@ -65,23 +142,48 @@ const RentAdd = () => {
       <WithRightArrow title="建筑面积" placeholder="请输入建筑面积" input={(val) => setArea(val)} rightText="m²"></WithRightArrow>
       <WithRightArrow
         onRightClick={() => {
+          setCurrentSelect('roomtype')
+          setPickerList([roomTypeData])
           setVisible(true)
         }}
         title="户型"
         rightArrow
-        rightText="请选择"
+        rightText={roomtypeText}
       ></WithRightArrow>
 
-      <WithRightArrow title="所在楼层" rightArrow rightText="请选择"></WithRightArrow>
-      <WithRightArrow title="朝向" rightArrow rightText="请选择"></WithRightArrow>
+      <WithRightArrow
+        onRightClick={() => {
+          setCurrentSelect('floor')
+          setPickerList([floorData])
+          setVisible(true)
+        }}
+        title="所在楼层"
+        rightArrow
+        rightText={floorText}
+      ></WithRightArrow>
+      <WithRightArrow
+        onRightClick={() => {
+          setCurrentSelect('position')
+          setPickerList([orientedData])
+          setVisible(true)
+        }}
+        title="朝向"
+        rightArrow
+        rightText={positionText}
+      ></WithRightArrow>
       <WithRightArrow title="房屋标题" />
       <Input placeholder="请输入标题(例如:整租小区名2室5000元)" value={title} onChange={(val) => setTitle(val)}></Input>
       <WithRightArrow title="房屋图像" />
       <div className={styles.uploadImg}>
-        <ImageUploader value={img} onChange={setImg} upload={uploadImg} />
+        <ImageUploader maxCount={6} multiple value={img} onChange={setImg} upload={uploadImg} />
       </div>
       <WithRightArrow title="房屋配置" />
-      <HouseConfig />
+      <HouseConfig
+        select
+        onChange={(val) => {
+          setHouseConfigValue(val)
+        }}
+      />
       <WithRightArrow title="房屋描述" />
       <div className={styles.desc}>
         <textarea
@@ -98,7 +200,38 @@ const RentAdd = () => {
         <Button>取消</Button>
         <Button color="success">提交</Button>
       </div>
+      {/* <div style={visible ? {} : { display: 'none' }}> */}
+      {!visible ? null : (
+        <>
+          {/* 弹出选择框 */}
+          <div className={styles.alert}>
+            <PickerView
+              columns={pickerList}
+              value={() => {
+                if (currentSelect === 'roomtype') return roomtype
+                if (currentSelect === 'floor') return floor
+                if (currentSelect === 'position') return position
+              }}
+              onChange={(val) => {
+                if (currentSelect === 'roomtype') {
+                  return setRoomtype(val)
+                }
+                if (currentSelect === 'floor') return setFloor(val)
+                if (currentSelect === 'position') return setPosition(val)
+              }}
+            />
+          </div>
+          {/* 遮罩层 */}
+          <div
+            className={styles.mask}
+            onClick={() => {
+              setVisible(false)
+            }}
+          ></div>
+        </>
+      )}
     </div>
+    // </div>
   )
 }
 export default RentAdd
